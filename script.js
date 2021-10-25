6b1b803fb3e08b64aca454142c497bef4ae926ad
Max Ivanskyi <m.ivanskiy@gmail.com>
2021-10-25 15:44:54 +0300

    new commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const requestAjax = (link, callBack) => {
 }
 
 const addZones = zones => {
-    const select = document.querySelector('#timeZoneSelect');
+    const select = document.querySelector('#select-timezone');
     const options = zones.map(zone => `<option value="${ zone }">${ zone }</option>`).join('');
     select.innerHTML = options;
 
@@ -19,26 +19,63 @@ const addZones = zones => {
 const onSelect = zone => {
     loadTime(zone.target.value);
 }
-const loadTime = zone => {
+const loadTime = (zone) => {
     const zoneLink = `${ linkZones }${ zone }`;
     requestAjax(zoneLink, displayTime);
 }
 
 const displayTime = zone => {
-    const timeWrapper = document.querySelector('.selectedTime');
-    const time = zone.datetime;
-    const regDateTime = /.*T(\d{2}:\d{2}):\d{2}\..*/;
-    const cleanTime = time.replace(regDateTime, '$1');
-    const result = `<h2>${ zone.timezone }: <span>${ cleanTime }</span></h2>`;
-    timeWrapper.insertAdjacentHTML("afterbegin", result);
+    const zoneTitle = document.querySelector('.zone-title');
+    const zoneTime = document.querySelector('.zone-time');
+    const zoneInfo = document.querySelector('.zone-info');
 
-    setMemo(zone.timezone);
+    const timeZone = zone.timezone;
+    const regZone = /.*\/(.*)/;
+    const cleanZone = timeZone.replace(regZone, '$1');
+
+    const week = zone.week_number;
+    const dyaOfYear = zone.day_of_year;
+    const time = zone.datetime;
+
+    const regDateDate = /(\d{4}-\d{2}-\d{2}).*/;
+    const cleanDate = time.replace(regDateDate, '$1');
+
+    const regDateTime = /.*T(\d{2}:\d{2}:\d{2})\..*/;
+    const cleanTime = time.replace(regDateTime, '$1');
+
+    let day = zone.day_of_week;
+    const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    for(let i = 1; i <= days.length; i += 1){
+        if(i == day){
+            day = days[i-1];
+        }
+    }
+
+    zoneTitle.innerHTML = `${ cleanZone } time`;
+    zoneTime.innerHTML = cleanTime;
+    zoneInfo.innerHTML = `<p>${ cleanDate }, ${ day }, week ${ week }</p>`
 }
 
-const setMemo = zone => {
-    const zones = new Set(JSON.parse(localStorage.getItem('timezones') ?? '[]'));
-    zones.add(zone);
-    localStorage.setItem('timezones', JSON.stringify([...zones]));
+
+const addButtons = zones => {
+    const popularZones = document.querySelector('.popular-zones');
+    for(let i = 0; i < zones.length; i += 1){
+        const zone = zones[i];
+        const regZone = /.*\/(.*)/;
+        const city = zone.replace(regZone, '$1');
+        popularZones.insertAdjacentHTML('beforeEnd', `<li class="zone-btn" data-zone="${ zone }">${ city }<span>time</span></li>`);
+    }
+
+    const buttons = document.getElementsByClassName('zone-btn');
+    
+    for(let i = 0; i < buttons.length; i += 1){
+        buttons[i].addEventListener('click', getButtonData);
+    };    
+}
+
+function getButtonData(){
+    const buttonZone = this.getAttribute("data-zone");
+    loadTime(buttonZone);
 }
 
 const startApp = () => {
@@ -46,7 +83,9 @@ const startApp = () => {
 }
 
 (() => {
-    const zones = JSON.parse(localStorage.getItem('timezones') ?? '[]');
-    zones.forEach(loadTime);
     startApp();
+    loadTime('Europe/Kiev');
+
+    const buttonsZones = ["America/Los_Angeles","America/New_York","Europe/London","Europe/Paris","Europe/Kiev","Asia/Tokyo"]
+    addButtons(buttonsZones);    
 })();
